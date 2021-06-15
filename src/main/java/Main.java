12b0ba6fe34446d5df9b6fbce1b5a5acb5cdce28
wang.xander <wang.xander@gmail.com>
2021-06-14 19:00:11 -0700

    Removed redundent message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,10 @@ import com.zombiecastlerush.util.Game;
 
 class Main {
     public static void main(String[] args){
-        System.out.println("Zombie Castle Rush Test");
+        System.out.println("Zombie Castle Rush Demo");
         Game ZombieCastleRush = Game.getInstance();
-        //TODO: temporary solution; we can handle exception here
         try {
             ZombieCastleRush.start();
-            //TODO: ask prompter to stop the game
-            // rushHour.stop();
-            System.out.println("Timer is up. The end ...");
         } catch (JacksonException je){
             System.out.println(je.getMessage());
         }
