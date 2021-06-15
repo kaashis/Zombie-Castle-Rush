34bcfb8b61f037fe70617ac516ddd24d5521cf65
wang.xander <wang.xander@gmail.com>
2021-06-15 05:21:58 -0700

    Added Coordinates in json output

--- src/main/java/com/zombiecastlerush/entity/Entity.java
+++ src/main/java/com/zombiecastlerush/entity/Entity.java
@@ -1,11 +1,22 @@
 package com.zombiecastlerush.entity;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.zombiecastlerush.building.Inventory;
 
+@JsonPropertyOrder({"name", "coordinates", "inventory"})
 public class Entity {
     private String name;
     private Inventory inventory = new Inventory();
     private String description;
+    private Coordinates coordinates = new Coordinates(-1,-1);
+
+    public void setCoordinates(int x, int y){
+        this.coordinates.setCoordinates(x,y);
+    }
+
+    public Coordinates getCoordinates(){
+        return this.coordinates;
+    }
 
     public Inventory getInventory() {
         return inventory;
